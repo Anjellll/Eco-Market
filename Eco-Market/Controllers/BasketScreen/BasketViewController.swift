aa1448f76610698374e8c994bcd0a3e0611dade6
Anjella <anjellakattoobekova@gmail.com>
2023-12-26 22:32:05 +0600

    added labels for cleaning and the title basket

--- Eco-Market/Controllers/BasketScreen/BasketViewController.swift
+++ Eco-Market/Controllers/BasketScreen/BasketViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class BasketViewController: UIViewController {
 
+    private lazy var clearLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Oчистить"
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var basketLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Корзина"
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textAlignment = .center
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
+    }
+}
 
-        view.backgroundColor = .red
-        navigationItem.title = "Корзина"
+extension BasketViewController {
+    private func setUpUI() {
+        setUpSubviews()
+        setUpConstraints()
+    }
+    
+    private func setUpSubviews() {
+        view.addSubview(clearLabel)
+        view.addSubview(basketLabel)
+    }
+    
+    private func setUpConstraints() {
+        clearLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(57)
+            $0.left.equalToSuperview().offset(16)
+            $0.width.equalTo(82)
+            $0.height.equalTo(18)
+        }
+        
+        basketLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(57)
+            $0.width.equalTo(80)
+            $0.height.equalTo(18)
+        }
     }
 }
